374b1aae51b53d2341ea53c8a054b99ceade8d43
flolikalka-hub <flolikalka@gmail.com>
2026-04-08 15:13:40 +0300

    новый бэкап

--- app/src/main/java/com/example/plantmap/db/DatabaseHelper.java
+++ app/src/main/java/com/example/plantmap/db/DatabaseHelper.java
@@ -10,7 +10,7 @@ import java.io.*;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DB_NAME = "PlantMap_DB.db";
-    private static final int DB_VERSION = 23;
+    private static final int DB_VERSION = 24;
 
     private final Context context;
     private String dbPath;
